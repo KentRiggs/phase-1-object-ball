0835f289e627b1b59d63a7b6ce4e9fb8a514fdb0
kent riggs <k2r3@proton.me>
2024-01-09 13:40:48 -0700

    added first player

---
+++ src/00-objectball.js
@@ -0,0 +1,24 @@
+function gameObject() {
+    return {
+      team: {
+        home: {
+          teamName: "Brooklyn Nets",
+          colors: ["black", "white"],
+        },
+        players: {
+          player1: {
+            name: "Alan Anderson",
+            number: "0",
+            shoe: "16",
+            points: "22",
+            rebounds: "12",
+            assists: "12",
+            steals: "3",
+            blocks: "1",
+            "slam dunks": "1",  
+          },
+        },
+      },
+    };
+  }
+  
